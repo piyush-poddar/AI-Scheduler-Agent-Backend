64e1e127a3409bf41854683b6d6da4b0df5672c2
piyush-poddar <contactme.piyush@gmail.com>
2025-06-24 04:42:18 +0530

    Updating code

--- calendar_service.py
+++ calendar_service.py
@@ -21,15 +21,15 @@ def get_calendar_service():
     with open('token.json', 'r') as token:
         creds = Credentials.from_authorized_user_info(json.load(token), SCOPES)
 
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+    #         creds = flow.run_local_server(port=0)
 
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+    #     with open('token.json', 'w') as token:
+    #         token.write(creds.to_json())
 
     service = build('calendar', 'v3', credentials=creds)
     return service
